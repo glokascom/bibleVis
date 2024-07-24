d7e0ac44a2166cc0ca61ffaf24f762ec7486f449
DenisArger <den.arger@gmail.com>
2024-07-24 13:12:19 +0300

    fix: delete unuswd folder for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
