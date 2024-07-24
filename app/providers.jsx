b41ca0cb63f4ba7e5b6e4ad815bbe2e2c14c1a79
DenisArger <den.arger@gmail.com>
2024-07-24 19:03:14 +0300

    fix: edit setting vscode and eslint

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,5 +1,5 @@
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react"
 
 export function Providers({ children }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return <NextUIProvider>{children}</NextUIProvider>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,4 +11,4 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [nextui()],
-};
+}
