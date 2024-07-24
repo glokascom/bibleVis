9f45661d17b661d678ff34f14918a2411cc4a311
DenisArger <den.arger@gmail.com>
2024-07-24 19:07:16 +0300

    fix: singleQuote: true

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,4 +1,4 @@
-import { NextUIProvider } from "@nextui-org/react"
+import { NextUIProvider } from '@nextui-org/react'
 
 export function Providers({ children }) {
   return <NextUIProvider>{children}</NextUIProvider>
