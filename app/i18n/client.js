899fdd15ed0e49a56bffdb21ef82e05ddf218a10
DenisArger <den.arger@gmail.com>
2024-07-29 23:19:41 +0300

    fix: transfer page on server

--- app/i18n/client.js
+++ app/i18n/client.js
@@ -23,8 +23,6 @@ i18next
   .init({
     ...getOptions(),
     lng: undefined, // let detect the language on client side
-    saveMissing: true,
-    updateMissing: true,
     detection: {
       order: ['path', 'htmlTag', 'cookie', 'navigator'],
     },
