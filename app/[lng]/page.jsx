c53f78e4d07d3cabf5b41bff6260f0262340db82
DenisArger <den.arger@gmail.com>
2024-07-30 14:33:20 +0300

    style: adв styles

--- app/[lng]/page.jsx
+++ app/[lng]/page.jsx
@@ -8,12 +8,16 @@ export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng)
 
   return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1>{t('title')}</h1>
-        <ButtonsBlock t={t} />
-        <Link href="/about">About</Link>
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
+      <div className="cursor-pointer bg-blue-500 p-4 text-white">
+        <h1 className="text-xl font-bold">{t('title')}</h1>
       </div>
-    </>
+      <Link href="/about">
+        <div className="cursor-pointer rounded-lg bg-green-500 p-4 text-white hover:bg-green-600">
+          <span className="text-lg font-semibold">About</span>
+        </div>
+      </Link>
+      <ButtonsBlock t={t} />
+    </div>
   )
 }
