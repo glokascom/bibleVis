2e43334e8aafa9d840c2b04735c65f83e0d8dace
DenisArger <den.arger@gmail.com>
2024-08-08 15:37:53 +0300

    fix: edit cookie deprecated

--- app/supabase/middleware.js
+++ app/supabase/middleware.js
@@ -1,27 +1,29 @@
 import { NextResponse } from 'next/server'
 
-import { createServerClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 
 export function supabaseMiddleware(request) {
   const response = NextResponse.next({ request: { headers: request.headers } })
 
-  const supabase = createServerClient(
+  const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get: (name) => request.cookies.get(name)?.value,
-        set: (name, value, options) => {
-          request.cookies.set({ name, value, ...options })
-          response.cookies.set({ name, value, ...options })
-        },
-        remove: (name, options) => {
-          request.cookies.set({ name, value: '', ...options })
-          response.cookies.set({ name, value: '', ...options })
-        },
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   )
 
-  return { supabase, response }
+  const cookies = request.cookies
+  const cookieOptions = { path: '/', sameSite: 'lax', httpOnly: true }
+
+  function getCookie(name) {
+    return cookies.get(name)?.value || ''
+  }
+
+  function setCookie(name, value, options = cookieOptions) {
+    response.cookies.set(name, value, options)
+  }
+
+  function removeCookie(name, options = cookieOptions) {
+    response.cookies.set(name, '', { ...options, maxAge: -1 })
+  }
+
+  return { supabase, response, getCookie, setCookie, removeCookie }
 }
