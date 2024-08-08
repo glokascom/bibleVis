8bcdb9dd3a8c4f634035aa8a0e2bdc2fb48a7899
Vorobusec <timur.yancev08@gmail.com>
2024-08-08 10:31:03 +0500

    WIP browsers version

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
+import { Button } from "@nextui-org/button";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Checkbox } from "@nextui-org/checkbox";
 import ship from "../public/ship.png";
 import logo from "../public/logo.svg";
 import continueWithGoogle from "../public/continue-with-google.svg";
@@ -9,7 +12,7 @@ import singInWithGoogle from "../public/sing-in-with-google.svg";
 
 const SignUpForm = () => (
   <form>
-    <div className="mb-6">
+    <div className="mb-4">
       <label
         htmlFor="username"
         className="block text-sm font-medium text-gray-700"
@@ -22,7 +25,7 @@ const SignUpForm = () => (
         className="mt-1 p-3 block w-full border border-gray-300 rounded-md"
       />
     </div>
-    <div className="mb-6">
+    <div className="mb-4">
       <label
         htmlFor="email"
         className="block text-sm font-medium text-gray-700"
@@ -35,7 +38,7 @@ const SignUpForm = () => (
         className="mt-1 p-3 block w-full border border-gray-300 rounded-md"
       />
     </div>
-    <div className="mb-6">
+    <div className="mb-4">
       <label
         htmlFor="password"
         className="block text-sm font-medium text-gray-700"
@@ -48,18 +51,18 @@ const SignUpForm = () => (
         className="mt-1 p-3 block w-full border border-gray-300 rounded-md"
       />
     </div>
-    <button
+    <Button
       type="submit"
       className="w-full bg-green-500 text-white py-3 px-4 rounded-md text-lg"
     >
       Join
-    </button>
+    </Button>
   </form>
 );
 
 const LoginForm = () => (
   <form>
-    <div className="mb-6">
+    <div className="mb-4">
       <label
         htmlFor="email"
         className="block text-sm font-medium text-gray-700"
@@ -72,7 +75,7 @@ const LoginForm = () => (
         className="mt-1 p-3 block w-full border border-gray-300 rounded-md"
       />
     </div>
-    <div className="mb-6">
+    <div className="mb-4">
       <label
         htmlFor="password"
         className="block text-sm font-medium text-gray-700"
@@ -85,12 +88,25 @@ const LoginForm = () => (
         className="mt-1 p-3 block w-full border border-gray-300 rounded-md"
       />
     </div>
-    <button
+    <div className="flex my-2 text-black">
+      <Checkbox radius="sm" color="primary"></Checkbox>
+      <p>Remember me</p>
+    </div>
+    <div className="flex my-2">
+      <p className="text-gray-400">
+        This site is protected by reCAPTCHA and the Google
+        <span className="text-green-500 inline"> Privacy Policy</span> and
+        <span className="text-green-500 inline"> Terms of Service </span>
+        apply.
+      </p>
+    </div>
+
+    <Button
       type="submit"
       className="w-full bg-green-500 text-white py-3 px-4 rounded-md text-lg"
     >
       Log in
-    </button>
+    </Button>
   </form>
 );
 
@@ -109,59 +125,65 @@ export default function Register() {
         />
         <div className="absolute inset-0 bg-[#232323B2]"></div>
       </div>
-      <div className="relative z-10 flex items-center justify-center h-full">
+      <div className="relative z-10 flex items-center justify-center h-full ">
         <div className="w-4/5 h-4/5 flex bg-white bg-opacity-80 rounded-xl overflow-hidden shadow-lg">
-          <div className="w-1/3 p-8 flex flex-col justify-center">
-            <div className="flex justify-center mb-6">
+          <Card className="w-full md:w-1/3 p-8 flex flex-col justify-center bg-white">
+            <CardHeader className="flex justify-center mb-4">
               <Image src={logo} alt="Logo" width={150} height={150} />
-            </div>
+            </CardHeader>
+            <CardBody className="bg-white">
+              <div className="flex justify-center mb-4">
+                <h2
+                  className={`text-2xl font-bold cursor-pointer ${
+                    isSignUp ? "text-green-500" : "text-gray-500"
+                  }`}
+                  onClick={() => setIsSignUp(true)}
+                >
+                  Sign up
+                </h2>
+                <h2 className="mx-4 hidden md:inline">|</h2>
+                <h2
+                  className={`text-2xl font-bold cursor-pointer ${
+                    !isSignUp ? "text-green-500" : "text-gray-500"
+                  } hidden md:inline`}
+                  onClick={() => setIsSignUp(false)}
+                >
+                  Log in
+                </h2>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="w-full h-20 flex items-center justify-center p-0 rounded-md overflow-hidden border-none"
+                >
+                  <Image
+                    src={isSignUp ? singInWithGoogle : continueWithGoogle}
+                    alt="Google"
+                    className="w-90% h-90%"
+                    style={{ objectFit: "cover" }}
+                  />
+                </button>
+              </div>
 
-            <div className="flex justify-center mb-6">
-              <h2
-                className={`text-2xl font-bold cursor-pointer ${
-                  isSignUp ? "text-green-500" : "text-gray-500"
-                }`}
-                onClick={() => setIsSignUp(true)}
-              >
-                Sign up
-              </h2>
-              <h2 className="mx-4">|</h2>
-              <h2
-                className={`text-2xl font-bold cursor-pointer ${
-                  !isSignUp ? "text-green-500" : "text-gray-500"
-                }`}
-                onClick={() => setIsSignUp(false)}
-              >
-                Log in
-              </h2>
-            </div>
-            <div>
-              <button
-                type="button"
-                className="w-full h-20 flex items-center justify-center p-0 rounded-md overflow-hidden border-none"
-              >
-                <Image
-                  src={isSignUp ? singInWithGoogle : continueWithGoogle}
-                  alt="Google"
-                  className="w-90% h-90%"
-                  style={{ objectFit: "cover" }}
-                />
-              </button>
-            </div>
-
-            {isSignUp ? <SignUpForm /> : <LoginForm />}
-            <div className="text-center mt-4">
-              <button
-                onClick={() => setIsSignUp(!isSignUp)}
-                className="text-blue-500"
-              >
-                {isSignUp
-                  ? "Already have an account? Log in"
-                  : "Don't have an account? Sign up"}
-              </button>
-            </div>
-          </div>
-          <div className="w-2/3 relative">
+              {isSignUp ? (
+                <SignUpForm />
+              ) : (
+                <div className="hidden md:block">
+                  <LoginForm />
+                </div>
+              )}
+              <div className="text-center mt-1 ">
+                {isSignUp ? (
+                  ""
+                ) : (
+                  <Button className="text-green-500 bg-white hidden md:inline">
+                    Forgot password?
+                  </Button>
+                )}
+              </div>
+            </CardBody>
+          </Card>
+          <div className="w-2/3 relative hidden md:block">
             <Image
               src={ship}
               alt="Ship Image"
