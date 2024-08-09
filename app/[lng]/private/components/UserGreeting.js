c8ac13dbad37122251a4213669850e3f089f8f89
DenisArger <den.arger@gmail.com>
2024-08-09 12:14:45 +0300

    fix: edit UserGreeting and GET query for logout

--- app/[lng]/private/components/UserGreeting.js
+++ app/[lng]/private/components/UserGreeting.js
@@ -1,33 +1,17 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function UserGreeting({ email }) {
-  const router = useRouter()
-
-  const handleSignOut = async () => {
-    const response = await fetch('/api/auth/logout', {
-      method: 'POST',
-    })
-
-    if (response.ok) {
-      router.push('/login')
-    } else {
-      console.error('Error logging out')
-    }
-  }
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-8 text-center shadow-lg">
         <h1 className="text-2xl font-bold text-gray-800">Hi, {email}!</h1>
-        <p className="mt-4 text-gray-600">Welcome to the BibleVis website.</p>
-        <button
-          onClick={handleSignOut}
+        <p className="my-4 text-gray-600">Welcome to the BibleVis website.</p>
+        <Link
+          href="/api/auth/logout?redirectTo=/login"
           className="mt-6 rounded-lg bg-red-500 px-4 py-2 text-white transition duration-300 hover:bg-red-600"
         >
           Log out
-        </button>
+        </Link>
       </div>
     </div>
   )
--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -2,13 +2,15 @@ import { NextResponse } from 'next/server'
 
 import { createClient } from '@/app/supabase/server'
 
-export async function POST() {
-  const supabase = createClient()
+export async function GET(request) {
+  const url = new URL(request.url)
+  const redirectTo = url.searchParams.get('redirectTo')
 
+  const supabase = createClient()
   const { error } = await supabase.auth.signOut()
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
 
-  return NextResponse.json({ message: 'Logout successful' })
+  return NextResponse.redirect(`${url.origin}/${redirectTo}`)
 }
