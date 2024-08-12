bc23312cec15e5f3da0a5e5175898b1904fefa30
Vitek Foxprogs <foxprogs@gmail.com>
2024-08-12 09:06:52 +0400

    refactor: :coffin: remove link

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -19,26 +19,24 @@ const Page: FC<Props> = async ({ params: { lng } }) => {
       <div className="cursor-pointer bg-blue-500 p-4 text-white">
         <h1 className="text-xl font-bold">{t('title')}</h1>
       </div>
-      <Link href="/about">
-        <a className="cursor-pointer rounded-lg bg-green-500 p-4 text-white hover:bg-green-600">
-          <span className="text-lg font-semibold">About</span>
-        </a>
+      <Link
+        href="/about"
+        className="cursor-pointer rounded-lg bg-green-500 p-4 text-white hover:bg-green-600"
+      >
+        <span className="text-lg font-semibold">About</span>
       </Link>
       <ButtonsBlock t={t} />
-      <Link href="/doc">
-        <a className="cursor-pointer rounded-lg bg-purple-500 p-4 text-white hover:bg-purple-600">
-          <span className="text-lg font-semibold">API Documentation</span>
-        </a>
+      <Link
+        href="/doc"
+        className="cursor-pointer rounded-lg bg-purple-500 p-4 text-white hover:bg-purple-600"
+      >
+        <span className="text-lg font-semibold">API Documentation</span>
       </Link>
-      <Link href="/config">
-        <a className="cursor-pointer rounded-lg bg-red-500 p-4 text-white hover:bg-red-600">
-          <span className="text-lg font-semibold">Config</span>
-        </a>
-      </Link>
-      <Link href="/login">
-        <div className="cursor-pointer rounded-lg bg-yellow-500 p-4 text-white hover:bg-yellow-600">
-          <span className="text-lg font-semibold">Authentication</span>
-        </div>
+      <Link
+        href="/login"
+        className="cursor-pointer rounded-lg bg-yellow-500 p-4 text-white hover:bg-yellow-600"
+      >
+        <span className="text-lg font-semibold">Authentication</span>
       </Link>
     </div>
   )
