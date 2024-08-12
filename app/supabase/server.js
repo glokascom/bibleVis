4f933835e4bd5311a4a77385aac1be967aab2e99
DenisArger <den.arger@gmail.com>
2024-08-12 14:44:40 +0300

    fix: edit setting server and midleware

--- app/supabase/server.js
+++ app/supabase/server.js
@@ -10,23 +10,16 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value || ''
+        getAll() {
+          return cookieStore.getAll()
         },
-        set(name, value, options = { path: '/', sameSite: 'lax', httpOnly: true }) {
+        setAll(cookiesToSet) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
           } catch {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name, options = { path: '/', sameSite: 'lax', httpOnly: true }) {
-          try {
-            cookieStore.set({ name, value: '', ...options, maxAge: -1 })
-          } catch {
-            // The `delete` method was called from a Server Component.
+            // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
