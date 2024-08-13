67f2122865b577df3df8e1043d6eef2ab20ae314
DenisArger <den.arger@gmail.com>
2024-08-13 10:57:41 +0300

    fix: used ...props

--- app/[lng]/login/components/InputField.js
+++ app/[lng]/login/components/InputField.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-const InputField = React.forwardRef(function InputField(
-  { id, name, type, required, label, value, onChange },
-  ref
-) {
+const InputField = React.forwardRef(function InputField({ id, label, ...props }, ref) {
   return (
     <>
       <label htmlFor={id} className="text-lg text-gray-800">
@@ -11,12 +8,8 @@ const InputField = React.forwardRef(function InputField(
       </label>
       <input
         id={id}
-        name={name}
-        type={type}
         ref={ref}
-        required={required}
-        value={value}
-        onChange={onChange}
+        {...props}
         className="w-64 rounded-md border border-gray-300 px-4 py-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </>
