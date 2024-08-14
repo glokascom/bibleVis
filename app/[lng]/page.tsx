70dc4e9bbcc9292d267efc4b5aee1a2e045cda71
DenisArger <den.arger@gmail.com>
2024-08-14 14:05:22 +0300

    fix: delete ButtonsBlock

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 
 import Link from 'next/link'
 
-import ButtonsBlock from '@/app/components/ButtonsBlock'
 import { useTranslation } from '@/app/i18n'
 
 type Props = {
@@ -16,16 +15,6 @@ const Page: FC<Props> = async ({ params: { lng } }) => {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
-      <div className="cursor-pointer bg-blue-500 p-4 text-white">
-        <h1 className="text-xl font-bold">{t('title')}</h1>
-      </div>
-      <Link
-        href="/about"
-        className="cursor-pointer rounded-lg bg-green-500 p-4 text-white hover:bg-green-600"
-      >
-        <span className="text-lg font-semibold">About</span>
-      </Link>
-      <ButtonsBlock t={t} />
       <Link
         href="/doc"
         className="cursor-pointer rounded-lg bg-purple-500 p-4 text-white hover:bg-purple-600"
@@ -36,7 +25,7 @@ const Page: FC<Props> = async ({ params: { lng } }) => {
         href="/login"
         className="cursor-pointer rounded-lg bg-yellow-500 p-4 text-white hover:bg-yellow-600"
       >
-        <span className="text-lg font-semibold">Authentication</span>
+        <span className="text-lg font-semibold">{t('login')} </span>
       </Link>
     </div>
   )
