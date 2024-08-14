0ae0e5a436b1a33e67a747a235236a61c6819490
DenisArger <den.arger@gmail.com>
2024-08-14 23:44:53 +0300

    fix: used useParams for query uuid

--- app/[lng]/user/[uuid]/page.js
+++ app/[lng]/user/[uuid]/page.js
@@ -1,10 +1,9 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
 
 export default function UserDetail() {
-  const router = useRouter()
-  const { uuid } = router.query
+  const { uuid } = useParams()
 
   return <div className="text-red-500">User Page for UUID: {uuid}</div>
 }
