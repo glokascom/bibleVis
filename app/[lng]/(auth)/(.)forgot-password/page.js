329807a6da63a963d250ded5982e03cdb93aac29
DenisArger <den.arger@gmail.com>
2024-08-15 00:38:42 +0300

    fix: add h1 fiw show modal

--- app/[lng]/(auth)/(.)forgot-password/page.js
+++ app/[lng]/(auth)/(.)forgot-password/page.js
@@ -1,5 +1,10 @@
 import ForgotForm from '../(components)/ForgotForm'
 
 export default async function ForgotPage() {
-  return <ForgotForm />
+  return (
+    <>
+      <h1 className="text-3xl text-red-900">Modal</h1>
+      <ForgotForm />
+    </>
+  )
 }
--- app/[lng]/(auth)/(.)login/page.js
+++ app/[lng]/(auth)/(.)login/page.js
@@ -1,5 +1,10 @@
 import LoginForm from '../(components)/LoginForm'
 
 export default async function LoginPage() {
-  return <LoginForm />
+  return (
+    <>
+      <h1 className="text-3xl text-red-900">Modal</h1>
+      <LoginForm />
+    </>
+  )
 }
