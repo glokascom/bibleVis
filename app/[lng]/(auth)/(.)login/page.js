ac99e945e9c4a4f057c5977e132fd298e62cc1e4
DenisArger <den.arger@gmail.com>
2024-08-16 13:30:31 +0300

    fix: move componets in login page

--- app/[lng]/(auth)/(.)login/page.js
+++ app/[lng]/(auth)/(.)login/page.js
@@ -1,32 +1,9 @@
-'use client'
-
-import TagInput from '@/app/components/TagInput'
-
 import LoginForm from '../components/LoginForm'
 
-const initialTags = [
-  'Adobe Photoshop',
-  'CorelDRAW',
-  'Canva',
-  'Adobe Lightroom',
-  'Figma',
-  'Adobe Illustrator',
-  'Sketch',
-  'DALL-E',
-  'Midjourney',
-  'Stable Diffusion',
-  'Leonardo.AI',
-]
-
-export default function LoginPage() {
-  const handleTagsChange = (tags) => {
-    console.log('Теги изменены:', tags)
-  }
-
+export default async function LoginPage() {
   return (
     <>
       <h1 className="text-3xl text-red-900">Modal</h1>
-      <TagInput initialTags={initialTags} onTagsChange={handleTagsChange} />
       <LoginForm />
     </>
   )
--- app/[lng]/(auth)/login/page.js
+++ app/[lng]/(auth)/login/page.js
@@ -1,5 +1,32 @@
+'use client'
+
+import TagInput from '@/app/components/TagInput'
+
 import LoginForm from '../components/LoginForm'
 
-export default async function LoginPage() {
-  return <LoginForm />
+const initialTags = [
+  'Adobe Photoshop',
+  'CorelDRAW',
+  'Canva',
+  'Adobe Lightroom',
+  'Figma',
+  'Adobe Illustrator',
+  'Sketch',
+  'DALL-E',
+  'Midjourney',
+  'Stable Diffusion',
+  'Leonardo.AI',
+]
+
+export default function LoginPage() {
+  const handleTagsChange = (tags) => {
+    console.log('Теги изменены:', tags)
+  }
+
+  return (
+    <>
+      <TagInput initialTags={initialTags} onTagsChange={handleTagsChange} />
+      <LoginForm />
+    </>
+  )
 }
