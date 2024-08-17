5d3aebd5c2b54cdbcb62ba3087d50ae62546bf6f
Vitek Foxprogs <foxprogs@gmail.com>
2024-08-17 23:31:43 +0400

    test: :white_check_mark: fixed tests

---
+++ __tests__/BVAvatar.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+import { BVAvatar } from '../app/components/BVAvatar'
+
+describe('BVAvatar component', () => {
+  it('renders correctly', () => {
+    render(
+      <BVAvatar
+        as="button"
+        className="transition-transform"
+        name="Jason Hughes"
+        size="md"
+        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+      />
+    )
+
+    expect(screen.getByAltText('Jason Hughes')).toBeInTheDocument()
+    expect(screen.getByRole('img')).toBeInTheDocument()
+  })
+})
