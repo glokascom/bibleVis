26a4985acfcbcb4666a5e8e7210be36408a8ae72
Vitek Foxprogs <foxprogs@gmail.com>
2024-08-19 15:08:45 +0400

    refactor: :lipstick: update login and register page

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -3,23 +3,45 @@
 import { useState } from 'react'
 
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 import { Button } from '@nextui-org/button'
 import { Tab, Tabs } from '@nextui-org/tabs'
 
+import { login, signup } from '../actions/actionsSupabase'
 import { BVButton } from './BVButton'
 import { BVInput } from './BVInput'
 
 function AuthForm() {
   const [isVisible, setIsVisible] = useState(false)
   const [error, setError] = useState(false)
+  const [emailLogin, setEmailLogin] = useState('')
+  const [passwordLogin, setPasswordLogin] = useState('')
+
+  const [emailSignup, setEmailSignup] = useState('')
+  const [passwordSignup, setPasswordSignup] = useState('')
+  const [usernameSignup, setUsernameSignup] = useState('')
+
+  const { push } = useRouter()
 
   const toggleVisibility = () => setIsVisible(!isVisible)
+
+  const handleSignup = () => {
+    signup(emailSignup, passwordSignup, usernameSignup)
+      .then(() => push('/'))
+      .catch(() => setError(true))
+  }
+
+  const handleLogin = () => {
+    login(emailLogin, passwordLogin)
+      .then(() => push('/'))
+      .catch(() => setError(true))
+  }
   return (
     <>
       <div className="fixed bottom-0 left-0 right-0 top-0 z-40 flex items-center justify-center bg-black/40 backdrop-blur-md">
         <div className="z-50 flex h-[90vh] w-[90vw] flex-row overflow-hidden rounded-medium">
-          <div className="flex h-full w-full shrink-0 flex-col items-center bg-background p-5 md:w-[480px]">
+          <div className="flex h-full w-full shrink-0 flex-col items-center bg-background px-5 pb-0 pt-5 md:w-[480px]">
             <Image
               className="mb-10"
               src="/biblevis-logo.svg"
@@ -36,22 +58,20 @@ function AuthForm() {
                   cursor: 'bg-primary h-px',
                   tab: 'pb-7 font-medium text-lg h-auto',
                   tabContent: 'text-secondary-200',
-                  panel: 'shadow-none',
+                  panel:
+                    'shadow-none  scroll-gutter flex-auto overflow-auto pb-10 pl-5 pr-3.5',
                   base: 'mx-5',
                 }}
               >
-                <Tab
-                  key="sign-up"
-                  title="Sign up"
-                  className="flex-auto overflow-scroll px-5 pb-5"
-                >
+                <Tab key="sign-up" title="Sign up">
                   <Button
                     variant="bordered"
                     color="default"
                     size="lg"
-                    className="relative mb-3 mt-7 h-14 border-1 hover:shadow-medium"
+                    className="relative mb-2 mt-5 h-14 border-1 hover:shadow-medium"
                     fullWidth
                     radius="full"
+                    onClick={() => push('/api/auth/google')}
                   >
                     <Image
                       src="/google.svg"
@@ -73,6 +93,8 @@ function AuthForm() {
                     size="sm"
                     isRequired
                     errorMessage={'Username already exists'}
+                    value={usernameSignup}
+                    onChange={(e) => setUsernameSignup(e.target.value)}
                     isInvalid={error}
                   />
                   <label htmlFor="email" className="mb-2 text-medium font-medium">
@@ -84,6 +106,8 @@ function AuthForm() {
                     size="sm"
                     isRequired
                     errorMessage={'Unsupported email format'}
+                    value={emailSignup}
+                    onChange={(e) => setEmailSignup(e.target.value)}
                     isInvalid={error}
                   />
                   <label htmlFor="password" className="mb-2 text-medium font-medium">
@@ -92,6 +116,8 @@ function AuthForm() {
                   <BVInput
                     variant="bordered"
                     size="sm"
+                    value={passwordSignup}
+                    onChange={(e) => setPasswordSignup(e.target.value)}
                     endContent={
                       <button
                         className="focus:outline-none"
@@ -125,22 +151,103 @@ function AuthForm() {
                     This site is protected by reCAPTCHA and the Google{' '}
                     <a target="_blank" href="#" className="text-primary hover:underline">
                       Privacy Policy
-                    </a>{' '}
-                    and{' '}
+                    </a>
+                    and
                     <a target="_blank" href="#" className="text-primary hover:underline">
                       Terms of Service
-                    </a>{' '}
+                    </a>
                     apply.
                   </p>
-                  <BVButton fullWidth onClick={() => setError(true)}>
+                  <BVButton fullWidth onClick={handleSignup}>
                     Join
                   </BVButton>
                 </Tab>
                 <Tab key="log-in" title="Log in">
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur.
+                  <Button
+                    variant="bordered"
+                    color="default"
+                    size="lg"
+                    className="relative mb-2 mt-5 h-14 border-1 hover:shadow-medium"
+                    fullWidth
+                    radius="full"
+                    onClick={() => push('/api/auth/google')}
+                  >
+                    <Image
+                      src="/google.svg"
+                      alt="google"
+                      width={23}
+                      height={22}
+                      className="absolute left-6 md:left-12"
+                    />
+                    Continue with Google
+                  </Button>
+                  <div className="divider my-5 flex items-center text-center before:mr-7 after:ml-7">
+                    <span className="text-medium font-medium text-secondary">or</span>
+                  </div>
+                  <label htmlFor="email" className="mb-2 text-medium font-medium">
+                    *Email
+                  </label>
+                  <BVInput
+                    type="email"
+                    variant="bordered"
+                    size="sm"
+                    value={emailLogin}
+                    onChange={(e) => setEmailLogin(e.target.value)}
+                    isRequired
+                    errorMessage={'Unsupported email format'}
+                    isInvalid={error}
+                  />
+                  <label htmlFor="password" className="mb-2 text-medium font-medium">
+                    *Password
+                  </label>
+                  <BVInput
+                    variant="bordered"
+                    size="sm"
+                    value={passwordLogin}
+                    onChange={(e) => setPasswordLogin(e.target.value)}
+                    endContent={
+                      <button
+                        className="focus:outline-none"
+                        type="button"
+                        onClick={toggleVisibility}
+                        aria-label="toggle password visibility"
+                      >
+                        {isVisible ? (
+                          <Image
+                            src={'/eye-open.svg'}
+                            alt="eye open"
+                            width={18}
+                            height={18}
+                            className="mr-4 h-[38px] w-[38px] p-2"
+                          />
+                        ) : (
+                          <Image
+                            src={'/eye-close.svg'}
+                            alt="eye close"
+                            width={16}
+                            height={16}
+                            className="mr-4 h-9 w-9 p-2"
+                          />
+                        )}
+                      </button>
+                    }
+                    type={isVisible ? 'text' : 'password'}
+                    isRequired
+                  />
+                  <p className="my-4 text-small text-secondary-200">
+                    This site is protected by reCAPTCHA and the Google{' '}
+                    <a target="_blank" href="#" className="text-primary hover:underline">
+                      Privacy Policy
+                    </a>
+                    and
+                    <a target="_blank" href="#" className="text-primary hover:underline">
+                      Terms of Service
+                    </a>
+                    apply.
+                  </p>
+                  <BVButton fullWidth onClick={handleLogin}>
+                    Join
+                  </BVButton>
                 </Tab>
               </Tabs>
             </div>
