d8a891bcbab7f99fba25993fada176710cd0c069
DenisArger <den.arger@gmail.com>
2024-08-20 16:56:42 +0300

    feat: add updateUsername

--- app/[lng]/user/[uuid]/actions/userService.js
+++ app/[lng]/user/[uuid]/actions/userService.js
@@ -1,3 +1,4 @@
+import { getUser } from '@/app/actions/getUser'
 import { supabaseService } from '@/app/supabase/service'
 
 export async function getUsernameById(userId) {
@@ -22,7 +23,7 @@ export async function getAvatarFilePathById(userId) {
     throw new Error('Error fetching avatar file path: ' + error.message)
   }
 
-  return data[0].avatar_file_path || null
+  return data[0]?.avatar_file_path || null
 }
 
 export async function getCoverFilePathById(userId) {
@@ -35,7 +36,7 @@ export async function getCoverFilePathById(userId) {
     throw new Error('Error fetching cover file path: ' + error.message)
   }
 
-  return data[0].cover_file_path || null
+  return data[0]?.cover_file_path || null
 }
 
 export async function getEmailFilePathById(userId) {
@@ -47,5 +48,44 @@ export async function getEmailFilePathById(userId) {
     throw new Error('Error fetching email: ' + error.message)
   }
 
-  return data[0].email || null
+  return data[0]?.email || null
+}
+
+export async function updateUsername(userId, newUsername) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { error } = await getUser()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new Error('User is not authenticated.')
+  }
+
+  if (!newUsername) {
+    throw new Error('New username cannot be empty.')
+  }
+
+  // Проверяем, существует ли уже новый username в базе данных
+  const { data: existingUsers, error: checkError } = await supabaseService
+    .from('users')
+    .select('id')
+    .eq('username', newUsername)
+
+  if (checkError) {
+    throw new Error('Error checking username existence: ' + checkError.message)
+  }
+
+  if (Array.isArray(existingUsers) && existingUsers.length > 0) {
+    throw new Error('Username is already taken.')
+  }
+
+  const { data, error: updateError } = await supabaseService
+    .from('users')
+    .update({ username: newUsername })
+    .eq('id', userId)
+
+  if (updateError) {
+    throw new Error('Error updating username: ' + updateError.message)
+  }
+
+  return data
 }
