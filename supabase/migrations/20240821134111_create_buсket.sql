acaff18d30812666d8dabbd1c0c39dccbe31060e
DenisArger <den.arger@gmail.com>
2024-08-21 18:09:44 +0300

    fix: edit table users

--- supabase/migrations/20240821134111_create_buсket.sql
+++ supabase/migrations/20240821134111_create_buсket.sql
@@ -9,3 +9,12 @@ values
 -- Add unique constraint to email
 ALTER TABLE public.users
 ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+ALTER TABLE public.users
+  DROP COLUMN avatar_file_path,
+  DROP COLUMN cover_file_path;
+
+ALTER TABLE public.users
+  ADD COLUMN avatar_file_exists BOOLEAN DEFAULT FALSE,
+  ADD COLUMN cover_file_exists BOOLEAN DEFAULT FALSE;
