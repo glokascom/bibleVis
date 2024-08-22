fe0ba46fedb67ae879c93167118a3dac987703b1
DenisArger <den.arger@gmail.com>
2024-08-22 23:49:26 +0300

    feat: delete all object for backet

--- supabase/migrations/20240821134111_create_buсket.sql
+++ supabase/migrations/20240821134111_create_buсket.sql
@@ -1,9 +1,13 @@
+-- Удалить все объекты в бакете, если они есть
+DELETE FROM storage.objects WHERE bucket_id = 'profile';
+
 -- Удалить существующий бакет, если он есть
 DELETE FROM storage.buckets WHERE id = 'profile';
 
 -- Создать новый бакет с публичным доступом
 INSERT INTO storage.buckets (id, name, public)
 VALUES ('profile', 'profile', true);
+
 
 -- Add unique constraint to email
 ALTER TABLE public.users
