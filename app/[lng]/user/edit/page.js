4337111a3ef6abe4a9507675f30db715a609c304
DenisArger <den.arger@gmail.com>
2024-08-22 15:20:37 +0300

    feat: add getUser

--- app/[lng]/user/edit/page.js
+++ app/[lng]/user/edit/page.js
@@ -1,9 +1,20 @@
+import { getUser } from '@/app/actions/getUser'
+
+import { getUserInfoById } from './actions/userService'
 import ProfileCard from './components/ProfileCard'
 
-export default function EditUser() {
+export default async function EditUser() {
+  let userInfo = null
+  try {
+    const data = await getUser()
+    userInfo = await getUserInfoById(data.id)
+  } catch (err) {
+    console.error(err)
+  }
+
   return (
     <div className="mx-auto mt-5 w-full max-w-5xl">
-      <ProfileCard />
+      <ProfileCard userInfo={userInfo} />
     </div>
   )
 }
