2b363b317f496fc970e4b9771a32ec2fca360924
DenisArger <den.arger@gmail.com>
2024-08-26 11:01:59 +0300

    feat: connect getUser and getUserInfoByUsername for page user

--- app/(web)/[@username]/page.js
+++ app/(web)/[@username]/page.js
@@ -1,21 +1,23 @@
+import { getUser } from '@/app/actions/getUser'
+
+import { getUserInfoByUsername } from '../user/edit/actions/userService'
 import Cover from './components/Cover'
 import UserInfo from './components/UserInfo'
 
 export default async function UserDetail({ params }) {
   const username = decodeURIComponent(params['@username']).replace('@', '')
-  const user = { username: 'AlenaAenami' }
-  const isCurrentUser = username === user.username
-
+  const data = await getUser()
+  const userInfo = data.user
+  const isCurrentUser = username === userInfo.username
+  const followUserInfo = await getUserInfoByUsername(username)
   return (
-    <main className="mx-auto w-full max-w-[1806px] px-6 md:px-12">
-      <div className="mt-2.5 flex h-[400px] flex-col items-start gap-7 md:mt-9 md:flex-row md:gap-1">
-        <div className="max-w-7xl">
-          <Cover isCurrentUser={isCurrentUser} />
-        </div>
-        <div className="h-full w-full md:w-auto md:min-w-44 md:grow lg:min-w-96">
-          <UserInfo isCurrentUser={isCurrentUser} />
-        </div>
+    <div className="mt-2.5 flex h-[400px] flex-col items-start gap-7 md:mt-9 md:flex-row md:gap-1">
+      <div className="max-w-7xl">
+        <Cover isCurrentUser={isCurrentUser} followUserInfo={followUserInfo} />
       </div>
-    </main>
+      <div className="h-full w-full md:w-auto md:min-w-44 md:grow lg:min-w-96">
+        <UserInfo isCurrentUser={isCurrentUser} followUserInfo={followUserInfo} />
+      </div>
+    </div>
   )
 }
