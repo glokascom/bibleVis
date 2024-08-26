0bd31dad06ef04f856cf9e3b7d1b13242504f842
DenisArger <den.arger@gmail.com>
2024-08-26 12:12:03 +0300

    feat: add redirect for userInfo null

--- app/(web)/[@username]/page.js
+++ app/(web)/[@username]/page.js
@@ -1,3 +1,5 @@
+import { redirect } from 'next/navigation'
+
 import { getUser } from '@/app/actions/getUser'
 
 import { getUserInfoByUsername } from '../user/edit/actions/userService'
@@ -7,16 +9,27 @@ import UserInfo from './components/UserInfo'
 export default async function UserDetail({ params }) {
   const username = decodeURIComponent(params['@username']).replace('@', '')
   const data = await getUser()
-  const userInfo = data.user
+  const userInfo = data?.user
+
+  if (!userInfo) {
+    redirect(`/login?redirectedFrom=@${username}`)
+    return null
+  }
+
   const isCurrentUser = username === userInfo.username
   const followUserInfo = await getUserInfoByUsername(username)
+
   return (
     <div className="mt-2.5 flex h-[400px] flex-col items-start gap-7 md:mt-9 md:flex-row md:gap-1">
       <div className="max-w-7xl">
         <Cover isCurrentUser={isCurrentUser} followUserInfo={followUserInfo} />
       </div>
       <div className="h-full w-full md:w-auto md:min-w-44 md:grow lg:min-w-96">
-        <UserInfo isCurrentUser={isCurrentUser} followUserInfo={followUserInfo} />
+        <UserInfo
+          isCurrentUser={isCurrentUser}
+          userInfo={userInfo}
+          followUserInfo={followUserInfo}
+        />
       </div>
     </div>
   )
