48393feed73cfc7a0aa08fea4ef5f635173ee5f6
DenisArger <den.arger@gmail.com>
2024-08-26 11:01:10 +0300

    feat: add function getUserInfoByUsername

--- app/(web)/user/edit/actions/userService.js
+++ app/(web)/user/edit/actions/userService.js
@@ -11,3 +11,15 @@ export async function getUserInfoById(userId) {
   }
   return data[0] || null
 }
+
+export async function getUserInfoByUsername(username) {
+  const { data, error } = await supabaseService
+    .from('users')
+    .select('id,username, avatar_file_exists, cover_file_exists, email')
+    .eq('username', username)
+
+  if (error) {
+    throw new Error('Error fetching username: ' + error.message)
+  }
+  return data[0] || null
+}
