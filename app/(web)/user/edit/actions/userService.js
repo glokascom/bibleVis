f2790487b240fb9fd14651d61c1c561895690183
DenisArger <den.arger@gmail.com>
2024-08-27 13:46:22 +0300

    fix: edit getUserInfoById getUserInfoByUsername with view

--- app/(web)/user/edit/actions/userService.js
+++ app/(web)/user/edit/actions/userService.js
@@ -1,10 +1,16 @@
-import { supabaseService } from '@/app/supabase/service'
+import { createClient } from '@/app/supabase/server'
 
 export async function getUserInfoById(userId) {
-  const { data, error } = await supabaseService
-    .from('users')
-    .select('id, username, avatar_file_exists, cover_file_exists, email')
+  const supabase = createClient()
+
+  const { data, error } = await supabase
+    .from('private_user_view')
+    .select('*')
     .eq('id', userId)
+
+  if (error) {
+    throw new Error('Error fetching username: ' + error.message)
+  }
 
   if (error) {
     throw new Error('Error fetching username: ' + error.message)
@@ -13,9 +19,11 @@ export async function getUserInfoById(userId) {
 }
 
 export async function getUserInfoByUsername(username) {
-  const { data, error } = await supabaseService
-    .from('users')
-    .select('id,username, avatar_file_exists, cover_file_exists, total_followers')
+  const supabase = createClient()
+
+  const { data, error } = await supabase
+    .from('public_user_view')
+    .select('*')
     .eq('username', username)
 
   if (error) {
