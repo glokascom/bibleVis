1724d3b8273a831151743826a573af8aaf176c04
Vitek Foxprogs <foxprogs@gmail.com>
2024-08-28 21:59:54 +0400

    feat: :passport_control: WIP reset password

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -10,13 +10,27 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="p fixed bottom-0 left-0 right-0 top-0 z-40 flex items-center justify-center bg-black/40">
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-40 flex items-center justify-center bg-black/40">
       {children}
       <button
         onClick={onDismiss}
-        className="rounded-medium bg-secondary px-5 py-3 text-red-500"
+        className="absolute right-[calc(5vw_-_2.5rem_-_10px)] top-[5vh] flex h-10 w-10 items-center justify-center rounded-full bg-secondary-50 text-secondary"
       >
-        Close
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 2L8 8M14 14L8 8M8 8L14 2L2 14"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </button>
     </div>
   )
