d10ee42c7b9bd9eefc47738613000c0790186343
DenisArger <den.arger@gmail.com>
2024-08-28 12:36:48 +0300

    fix: fix for linter

--- app/(web)/[@username]/components/UserInfo.tsx
+++ app/(web)/[@username]/components/UserInfo.tsx
@@ -59,7 +59,7 @@ const UserInfo: React.FC<UserInfoProps> = ({
     <div className="border-0 md:h-full md:rounded-medium md:px-5 md:py-4 2xl:border 2xl:border-secondary-200">
       <div className="relative flex w-full flex-col items-center md:gap-3 2xl:gap-5">
         <BVAvatar
-          className="text-mega absolute bottom-0 left-0 h-14 w-14 md:relative md:bottom-auto md:left-auto md:h-20 md:w-20 2xl:h-36 2xl:w-36"
+          className="absolute bottom-0 left-0 h-14 w-14 text-mega md:relative md:bottom-auto md:left-auto md:h-20 md:w-20 2xl:h-36 2xl:w-36"
           src={imageSrc}
           alt={`${followUserInfo.username}'s avatar`}
         />
