3f2a49b4cb65e6d44dfbff947326dff05948448b
DenisArger <den.arger@gmail.com>
2024-08-29 17:08:14 +0300

    style: edit style for figma

--- app/components/CustomToast.tsx
+++ app/components/CustomToast.tsx
@@ -9,37 +9,47 @@ const CustomToast: React.FC<CustomToastProps> = ({ message, type }) => {
   switch (type) {
     case 'success':
       backgroundColor = 'bg-[#01AB6C4D]'
-      backgroundIconColor = 'bg-[#10CD87]'
+      backgroundIconColor = '#10CD87'
       break
     case 'error':
       backgroundColor = 'bg-[#AB01014D]'
-      backgroundIconColor = 'bg-[#EA4F57]'
+      backgroundIconColor = '#EA4F57'
       break
     case 'neutral':
     default:
       backgroundColor = 'bg-[#96969680]'
-      backgroundIconColor = 'bg-[#2D2D2E]'
+      backgroundIconColor = '#2D2D2E'
       break
   }
 
   return (
     <div
-      className={`flex w-full max-w-xs items-center justify-between rounded-lg p-4 shadow-md ${backgroundColor} min-h-[56px]`}
+      className={`flex w-full max-w-xs items-center justify-between rounded-lg p-4 shadow-md ${backgroundColor} min-h-[50px] min-w-[356px]`}
     >
       <div className="mr-4 h-full w-[2px] rounded bg-white"></div>
-      <div className="flex-grow font-medium text-white">{message}</div>
-      <div className={`ml-4 rounded-full text-white ${backgroundIconColor} p-1`}>
+      <div className="flex-grow text-sm font-medium text-white">{message}</div>
+      <div className="ml-4 p-1">
         <svg
+          width="26"
+          height="26"
+          viewBox="0 0 26 26"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-current"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          strokeWidth="2"
         >
+          <ellipse
+            cx="13"
+            cy="13"
+            rx="13"
+            ry="13"
+            transform="rotate(-180 13 13)"
+            fill={backgroundIconColor}
+          />
           <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M6.225 4.811a1 1 0 011.414 0L12 9.172l4.361-4.36a1 1 0 011.415 1.414L13.414 10.586l4.362 4.361a1 1 0 01-1.415 1.415L12 12l-4.361 4.362a1 1 0 01-1.415-1.415L10.586 10.586 6.225 6.225a1 1 0 010-1.414z"
+            d="M10 10L13 13M16 16L13 13M13 13L16 10L10 16"
+            stroke="white"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </div>
