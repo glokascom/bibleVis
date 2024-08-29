772964b468fb53d9bc6f0b0372b8cfd077776cf1
DenisArger <den.arger@gmail.com>
2024-08-29 12:51:31 +0300

    fix: edit url for storage

--- app/actions/getAvatars.js
+++ app/actions/getAvatars.js
@@ -1,12 +1,12 @@
 'use server'
 export const getAvatars = async (userData) => {
-  const takenAt = new Date().toISOString()
-  const avatarUrl = userData.avatar_file_exists
-    ? `${process.env.STORAGE_URL}/object/public/profile/${userData.id}/avatars/normal.jpg?date=${takenAt}`
+  const avatarUrl = userData.avatar_file_path
+    ? `${process.env.STORAGE_URL}/object/public/profile/${userData.avatar_file_path}`
     : null
-  const coverUrl = userData.cover_file_exists
-    ? `${process.env.STORAGE_URL}/object/public/profile/${userData.id}/covers/original.jpg?date=${takenAt}`
-    : '/cover.svg'
+
+  const coverUrl = userData.cover_file_path
+    ? `${process.env.STORAGE_URL}/object/public/profile/${userData.cover_file_path}`
+    : `/cover.svg`
 
   return { avatarUrl, coverUrl }
 }
