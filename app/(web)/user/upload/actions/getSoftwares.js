faac6b9ff8791d6802df0b2682ea326c9faaa02d
DenisArger <den.arger@gmail.com>
2024-09-05 09:30:18 +0300

    refactor: edit getDataFromTable

--- app/(web)/user/upload/actions/getSoftwares.js
+++ app/(web)/user/upload/actions/getSoftwares.js
@@ -1,31 +1,16 @@
 import { supabaseService } from '@/app/supabase/service'
 
-export async function getSoftwares() {
+export async function getDataFromTable(tableName) {
   try {
-    const { data, error } = await supabaseService.from('softwares').select('*')
+    const { data, error } = await supabaseService.from(tableName).select('*')
 
     if (error) {
-      console.error('Error fetching data:', error)
-      return { status: 500, error: 'Error fetching data' }
+      console.error(`Error fetching data from ${tableName}:`, error)
+      return { status: 500, error: `Error fetching data from ${tableName}` }
     }
     return { status: 200, data }
   } catch (error) {
-    console.error('Unexpected error:', error)
-    return { status: 500, error: 'Unexpected error' }
-  }
-}
-
-export async function getTags() {
-  try {
-    const { data, error } = await supabaseService.from('tags').select('*')
-
-    if (error) {
-      console.error('Error fetching data:', error)
-      return { status: 500, error: 'Error fetching data' }
-    }
-    return { status: 200, data }
-  } catch (error) {
-    console.error('Unexpected error:', error)
-    return { status: 500, error: 'Unexpected error' }
+    console.error(`Unexpected error fetching data from ${tableName}:`, error)
+    return { status: 500, error: `Unexpected error fetching data from ${tableName}` }
   }
 }
--- app/(web)/user/upload/page.js
+++ app/(web)/user/upload/page.js
@@ -1,9 +1,9 @@
-import { getSoftwares, getTags } from './actions/getSoftwares'
+import { getDataFromTable } from './actions/getSoftwares'
 import UploadImage from './components/UploadImage'
 
 export default async function EditUser() {
-  const softwareOptions = (await getSoftwares()).data
-  const tagsOptions = (await getTags()).data
+  const softwareOptions = (await getDataFromTable('softwares')).data
+  const tagsOptions = (await getDataFromTable('tags')).data
   return (
     <>
       <UploadImage softwareOptions={softwareOptions} tagsOptions={tagsOptions} />
