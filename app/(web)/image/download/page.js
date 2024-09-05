1438dfc8e56ec16511dd0d7ddc1496e8d610edce
DenisArger <den.arger@gmail.com>
2024-09-05 09:35:40 +0300

    fix: used tailwind style

--- app/(web)/image/download/page.js
+++ app/(web)/image/download/page.js
@@ -1,3 +1,4 @@
+//TODO: delete on release. This is just for testing
 'use client'
 
 import { useEffect, useState } from 'react'
@@ -7,7 +8,6 @@ import Image from 'next/image'
 const customLoader = ({ src }) => src
 
 const src = '4686d99d-32f7-4fe7-8df1-aa7bce0b5079/images/image_2024-08-30-142238.jpg'
-//TODO: delete on release. This is just for testing
 
 async function requestProcessedImage(src, width) {
   const response = await fetch('/api/process-image', {
@@ -51,7 +51,7 @@ export default function ImagesPage() {
   }, [imageWidths])
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', padding: '20px' }}>
+    <div className="flex flex-wrap gap-5 p-5">
       {imageWidths.map((width) => (
         <Image
           key={width}
@@ -60,6 +60,7 @@ export default function ImagesPage() {
           alt={`Image at width ${width}`}
           width={width}
           height={(width * 1400) / 1640}
+          className="rounded-lg shadow-md"
         />
       ))}
     </div>
