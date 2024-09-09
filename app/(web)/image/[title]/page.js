e42d678c7e8bdcc891cf0ed6395d4654d101b1d3
Pavel <braurs64@gmail.com>
2024-09-09 15:41:03 +0400

    feat: latest edits

--- app/(web)/image/[title]/page.js
+++ app/(web)/image/[title]/page.js
@@ -4,13 +4,7 @@ import { useEffect, useState } from 'react'
 
 import { useRouter } from 'next/navigation'
 
-import { Image } from '@nextui-org/image'
-
-import CreatorDetails from '@/app/components/CreatorDetails'
-import Description from '@/app/components/Description'
-import Download from '@/app/components/Download'
-import SoftwareUsed from '@/app/components/SoftwareUsed'
-import TagList from '@/app/components/TagList'
+import ImagePageContent from '@/app/components/ImagePageContent'
 
 import { getImages } from '../../[@username]/actions/images'
 
@@ -63,65 +57,9 @@ export default function ImagePage({ params }) {
       <div className="px-5">
         <h1 className="text-3xl font-bold text-blue-500 underline">UUID: {uuid}</h1>
         <p className="text-red-500">Search Text: {searchText}</p>
-
-        <div className="flex flex-col md:flex-row md:items-start">
-          <div className="flex h-56 items-center justify-center rounded-medium bg-secondary-50 p-2.5 md:h-[45rem] md:w-3/4">
-            <p className="h-full w-full content-center rounded-medium bg-secondary-100 text-center">
-              Image Here
-            </p>
-          </div>
-
-          <div className="rounded-medium md:w-1/4 md:bg-secondary-50 md:p-2.5">
-            <div className="flex flex-col gap-12 rounded-medium md:gap-5">
-              <div className="rounded-medium border bg-background p-5 shadow-small">
-                <Download />
-                <Description />
-                <CreatorDetails />
-              </div>
-
-              <div className="hidden rounded-medium border bg-background p-5 shadow-small md:block">
-                <SoftwareUsed />
-              </div>
-
-              <div className="rounded-medium border bg-background p-5 shadow-small">
-                <TagList />
-              </div>
-
-              <div className="hidden rounded-medium border bg-background p-5 shadow-small md:block">
-                <p className="font-bold">More by Author Name</p>
-                <div className="mt-5 md:grid md:grid-cols-3 md:gap-2">
-                  {relatedImages.map((image) => (
-                    <Image
-                      key={image.id}
-                      src={image.url}
-                      alt={image.title}
-                      isZoomed
-                      className="mt-5 md:mt-0"
-                      classNames={{ img: 'md:aspect-square' }}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
 
-      <div className="mt-12 border-t px-5 py-10 md:hidden">
-        <p className="font-bold">More by Author Name</p>
-        <div className="md:grid md:grid-cols-3 md:gap-2">
-          {relatedImages.map((image) => (
-            <Image
-              key={image.id}
-              src={image.url}
-              alt={image.title}
-              isZoomed
-              className="mt-5 md:mt-0"
-              classNames={{ img: 'md:aspect-square' }}
-            />
-          ))}
-        </div>
-      </div>
+      <ImagePageContent relatedImages={relatedImages} />
     </main>
   )
 }
--- app/components/Description.jsx
+++ app/components/Description.jsx
@@ -1,16 +1,6 @@
 import CopyButton from './CopyButton'
 import LikesCounter from './LikesCounter'
 
-const TextSection = ({ title, id, text }) => (
-  <>
-    <div className="flex">
-      <p className="mr-4 font-bold">{title}</p>
-      <CopyButton textToCopy={text} />
-    </div>
-    <p id={id}>{text}</p>
-  </>
-)
-
 const StatItem = ({ label, value }) => (
   <div className="flex justify-between font-medium">
     <p className="text-secondary-400">{label}</p>
@@ -30,33 +20,32 @@ function Description() {
     <div className="my-5 flex flex-col gap-5 border-y-1 py-5 text-small">
       <LikesCounter />
 
-      <div className="hidden flex-col gap-5 md:flex">
+      <div className="flex flex-col gap-5">
         <p className="text-large font-bold">Jonah is in the city</p>
-        <TextSection
-          title="Description"
-          id="description"
-          text="Lorem description ipsum dolor sit amet consectetur. Ipsum cras porttitor a enim gravida adipiscing et. Et et ornare urna tellus sagittis. Non vestibulum lectus id enim. Laoreet tincidunt nulla nunc tincidunt et consequat accumsan bibendum nibh."
-        />
-        <TextSection
-          title="Prompt"
-          id="prompt"
-          text="Lorem prompt ipsum dolor sit amet consectetur. Ipsum cras porttitor a enim gravida adipiscing et. Et et ornare urna tellus sagittis. Non vestibulum lectus id enim. Laoreet tincidunt nulla nunc tincidunt et consequat accumsan bibendum nibh."
-        />
+
+        <p>
+          Lorem description ipsum dolor sit amet consectetur. Ipsum cras porttitor a enim
+          gravida adipiscing et. Et et ornare urna tellus sagittis. Non vestibulum lectus
+          id enim. Laoreet tincidunt nulla nunc tincidunt et consequat accumsan bibendum
+          nibh.
+        </p>
+
+        <div className="flex">
+          <p className="mr-4 font-bold">Prompt</p>
+          <CopyButton textToCopy="Lorem prompt ipsum dolor sit amet consectetur. Ipsum cras porttitor a enim gravida adipiscing et. Et et ornare urna tellus sagittis. Non vestibulum lectus id enim. Laoreet tincidunt nulla nunc tincidunt et consequat accumsan bibendum nibh." />
+        </div>
+        <p>
+          Lorem prompt ipsum dolor sit amet consectetur. Ipsum cras porttitor a enim
+          gravida adipiscing et. Et et ornare urna tellus sagittis. Non vestibulum lectus
+          id enim. Laoreet tincidunt nulla nunc tincidunt et consequat accumsan bibendum
+          nibh.
+        </p>
       </div>
 
-      <div className="hidden flex-col gap-3 md:flex">
+      <div className="flex flex-col gap-3">
         {statistics.map((stat, index) => (
           <StatItem key={index} label={stat.label} value={stat.value} />
         ))}
-      </div>
-
-      <div className="md:hidden">
-        <p className="mb-5 text-large font-bold">Jonah is in the city</p>
-        <div className="flex flex-col gap-3">
-          {statistics.map((stat, index) => (
-            <StatItem key={index} label={stat.label} value={stat.value} />
-          ))}
-        </div>
       </div>
     </div>
   )
