29b0c5181eea16ff63cd0f47e81834f2e98e673c
DenisArger <den.arger@gmail.com>
2024-09-09 10:55:58 +0300

    fix: minor edits

--- app/(web)/[@username]/components/Gallery.js
+++ app/(web)/[@username]/components/Gallery.js
@@ -10,7 +10,7 @@ import ImageForGallery from '@/app/components/ImageForGallery'
 import { loadNextPage } from '../actions/imagesActions'
 
 function Gallery({ userId, followUserId, initialImages }) {
-  const [images, setImages] = useState(initialImages)
+  const [images, setImages] = useState(initialImages || [])
   const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(true)
   const [totalImages, setTotalImages] = useState(0)
@@ -50,12 +50,12 @@ function Gallery({ userId, followUserId, initialImages }) {
   return (
     <>
       <div className="mb-4 flex items-center">
-        <h3 className="mr-4 font-sans text-5xl text-secondary">Gallary</h3>
+        <h3 className="mr-4 font-sans text-5xl text-secondary">Gallery</h3>
         <span className="mt-6 font-sans text-base text-secondary">Images</span>
         <span className="ml-2 mt-7 text-secondary-500">{totalImages}</span>
       </div>
       <InfiniteScroll
-        dataLength={images.length}
+        dataLength={images?.length}
         next={loadMoreImages}
         hasMore={hasMore}
         loader={<h4>Loading...</h4>}
@@ -63,7 +63,7 @@ function Gallery({ userId, followUserId, initialImages }) {
       >
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 640: 3, 1280: 4 }}>
           <Masonry gutter="10px">
-            {images.map((image) => (
+            {images?.map((image) => (
               <div key={image.id}>
                 <ImageForGallery userId={userId} image={image} />
               </div>
