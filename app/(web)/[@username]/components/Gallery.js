977546f15993c1b2529583cd913538f311ee721b
DenisArger <den.arger@gmail.com>
2024-09-10 12:39:12 +0300

    fix: return initialize for gallуry

--- app/(web)/[@username]/components/Gallery.js
+++ app/(web)/[@username]/components/Gallery.js
@@ -10,15 +10,26 @@ import ImageForGallery from '@/app/components/ImageForGallery'
 import { loadNextPage } from '../actions/imagesActions'
 
 function Gallery({ userId, followUserId, initialImages }) {
-  const [images, setImages] = useState(initialImages || [])
+  const [images, setImages] = useState(initialImages)
   const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(true)
+  const [mounted, setMounted] = useState(false)
   const [totalImages, setTotalImages] = useState(0)
   const isLoadingRef = useRef(false)
 
   useEffect(() => {
     setImages(initialImages)
   }, [initialImages])
+
+  useEffect(() => {
+    const initialize = async () => {
+      await loadMoreImages()
+      setMounted(true)
+    }
+
+    initialize()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const loadMoreImages = async () => {
     if (isLoadingRef.current || !hasMore) return
@@ -47,15 +58,17 @@ function Gallery({ userId, followUserId, initialImages }) {
     isLoadingRef.current = false
   }
 
+  if (!mounted) return null
+
   return (
     <>
       <div className="mb-4 flex items-center">
-        <h3 className="mr-4 font-sans text-5xl text-secondary">Gallery</h3>
+        <h3 className="mr-4 font-sans text-5xl text-secondary">Gallary</h3>
         <span className="mt-6 font-sans text-base text-secondary">Images</span>
         <span className="ml-2 mt-7 text-secondary-500">{totalImages}</span>
       </div>
       <InfiniteScroll
-        dataLength={images?.length}
+        dataLength={images.length}
         next={loadMoreImages}
         hasMore={hasMore}
         loader={<h4>Loading...</h4>}
@@ -63,9 +76,9 @@ function Gallery({ userId, followUserId, initialImages }) {
       >
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 640: 3, 1280: 4 }}>
           <Masonry gutter="10px">
-            {images?.map((image) => (
+            {images.map((image) => (
               <div key={image.id}>
-                <ImageForGallery image={image} />
+                <ImageForGallery userId={userId} image={image} />
               </div>
             ))}
           </Masonry>
