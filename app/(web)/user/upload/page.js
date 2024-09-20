bcb4dcf8f7ccc3e5943d42f4af580517cc05da4a
DenisArger <den.arger@gmail.com>
2024-09-20 14:13:53 +0300

    fix: add dynamic render

--- app/(web)/user/upload/page.js
+++ app/(web)/user/upload/page.js
@@ -3,6 +3,7 @@ import { getUser } from '@/app/actions/getUser'
 import { getSoftwares, getTags } from './actions/getSoftwares'
 import UploadImage from './components/UploadImage'
 
+export const dynamic = 'force-dynamic'
 export default async function EditUser() {
   const softwareOptions = (await getSoftwares()).data
   const tagsOptions = (await getTags()).data
