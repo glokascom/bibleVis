e040cf9365154692df861da619a26adf41be6826
Vitek Foxprogs <foxprogs@gmail.com>
2024-09-23 12:57:14 +0400

    refactor: :bulb: remove comments

--- app/(full-width)/reset-password/page.js
+++ app/(full-width)/reset-password/page.js
@@ -5,7 +5,7 @@ import ResetForm from '@/app/components/ResetForm'
 
 export default async function ResetPassword({ searchParams }) {
   const headersList = headers()
-  const host = headersList.get('host') // Получаем хост
+  const host = headersList.get('host')
   const protocol = headersList.get('x-forwarded-proto') || 'http'
   const origin = `${protocol}://${host}`
   const code = searchParams['code']
