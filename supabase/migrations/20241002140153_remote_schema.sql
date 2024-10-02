bb6eda157a5b6a1872bf8581e544883b2fd8c1dd
Vitek Foxprogs <foxprogs@gmail.com>
2024-10-03 01:55:38 +0400

    fix: try to fix db error when build

--- supabase/migrations/20241002140153_remote_schema.sql
+++ supabase/migrations/20241002140153_remote_schema.sql
@@ -1,5 +1,5 @@
-alter table "auth"."identities" drop constraint "identities_pkey";
+-- alter table "auth"."identities" drop constraint "identities_pkey";
 
-drop index if exists "auth"."identities_pkey";
+-- drop index if exists "auth"."identities_pkey";
 
 
