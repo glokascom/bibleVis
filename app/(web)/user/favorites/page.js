9140c0d507f09a2bfdd50b03b08334b7e46ab413
Pavel <braurs64@gmail.com>
2024-11-20 15:51:04 +0400

    fix: fix deploy

--- app/(web)/user/favorites/page.js
+++ app/(web)/user/favorites/page.js
@@ -1,13 +1,43 @@
+import { Suspense } from 'react'
+
+import { redirect } from 'next/navigation'
+
+import { getUser } from '@/app/actions/getUser'
+
 import { getFavoriteCounts } from './actions/getFavoriteCounts'
 import FavoritesGallery from './components/FavoritesGallery'
 
+export const dynamic = 'force-dynamic'
+
 export default async function FavoritesPage() {
-  const { counters } = await getFavoriteCounts()
+  let user = null
+  let counters = {
+    total: 0,
+    aiGenerated: 0,
+    humanMade: 0,
+  }
+
+  try {
+    const result = await getUser()
+    if (result.error) throw result.error
+    user = result.user
+
+    if (!user) {
+      redirect('/login')
+    }
+
+    const favoritesResult = await getFavoriteCounts()
+    counters = favoritesResult.counters
+  } catch (err) {
+    console.error(err)
+  }
 
   return (
     <>
       <h1 className="mb-10 pt-14 text-5xl font-medium md:pt-24">liked images</h1>
-      <FavoritesGallery isAuthenticated={true} counters={counters} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FavoritesGallery isAuthenticated={!!user} counters={counters} />
+      </Suspense>
     </>
   )
 }
