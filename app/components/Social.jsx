68b03875eced48aee721596a59ae6f0cf0836f83
Vitalii-250885 <provitaleks@gmail.com>
2024-11-29 10:33:48 +0200

    fix(Social.jsx): correct icons and target

--- app/components/Social.jsx
+++ app/components/Social.jsx
@@ -4,20 +4,32 @@ import Link from 'next/link'
 function Social() {
   return (
     <div className="flex items-center">
-      <Link href="https://youtube.com/@levelbible?si=RU5mp2w7xO9QnkFp">
-        <Image src="/Youtube.svg" alt="youtube" />
+      <Link href="https://youtube.com/@levelbible?si=RU5mp2w7xO9QnkFp" target="_blank">
+        <Image src="/Youtube.svg" alt="youtube" height={28} width={28} />
       </Link>
-      <Link href="https://x.com/bibleviscom">
-        <Image src="/Twitter.svg" alt="twitter" className="p-2" />
+      <Link href="https://x.com/bibleviscom" target="_blank">
+        <Image
+          src="/Twitter.svg"
+          alt="twitter"
+          className="p-2.5"
+          height={32}
+          width={32}
+        />
       </Link>
-      <Link href="https://www.instagram.com/biblevis?igsh=MTZieDVmbWw1eDIybw==">
-        <Image src="/Instagram.svg" alt="instagram" />
+      <Link
+        href="https://www.instagram.com/biblevis?igsh=MTZieDVmbWw1eDIybw=="
+        target="_blank"
+      >
+        <Image src="/Instagram.svg" alt="instagram" height={28} width={28} />
       </Link>
-      <Link href="https://www.facebook.com/profile.php?id=61562437224271">
-        <Image src="/Facebook.svg" alt="facebook" />
+      <Link href="https://www.facebook.com/profile.php?id=61562437224281" target="_blank">
+        <Image src="/Facebook.svg" alt="facebook" height={28} width={28} />
       </Link>
-      <Link href="https://www.linkedin.com/showcase/biblevis/?viewAsMember=true">
-        <Image src="/Linkedin.svg" alt="linkedin" />
+      <Link
+        href="https://www.linkedin.com/showcase/biblevis/?viewAsMember=true"
+        target="_blank"
+      >
+        <Image src="/Linkedin.svg" alt="linkedin" height={28} width={28} />
       </Link>
     </div>
   )
